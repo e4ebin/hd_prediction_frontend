f75ce3f5031a041e8bcf75a4f986655da4a99c8f
e4ebin <68241489+e4ebin@users.noreply.github.com>
2023-03-18 11:00:52 +0530

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,29 @@
-js
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/index.vue')
+    },
+    {
+      path: '/predict',
+      name: 'predict',
+      component: () => import('../views/prediction.vue')
+    },
+    {
+      path: '/performance',
+      name: 'performance',
+      component: () => import('../views/performance.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/about.vue')
+    }
+  ]
+})
+
+export default router
